d747c6298ba99399295149ba18fcfa34fe7a0e8b
Abraham Kashua <160520559+KashuaAbraham@users.noreply.github.com>
2024-06-06 15:26:41 +0100

    Complete creation of a simple VPC using terraform

---
+++ Main.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "simple_vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = var.aws_availability_zone
+
+  tags = {
+    Name = "public_subnet"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main_igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "public_rt"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  default = "us-west-2"
+}
+
+variable "aws_availability_zone" {
+  default = "us-west-2a"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
